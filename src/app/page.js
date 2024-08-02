89055aef474aff94915dbb3defb23de410464304
Nina Le <ninal11@uci.edu>
2024-08-01 23:46:30 -0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@
 import Image from 'next/image'
 import {useState, useEffect} from 'react'
 import {firestore} from './firebase'
-import {Box, Typography} from '@mui/material'
-import {collection, deleteDoc, doc, getDocs, query} from 'firebase/firestore'
+import {Box, Button, Modal, Stack, TextField, Typography} from '@mui/material'
+import {collection, deleteDoc, doc, getDocs, getDoc, setDoc, query} from 'firebase/firestore'
 
 export default function Home() {
   const [inventory, setInventory] = useState([])
@@ -14,7 +14,7 @@ const updateInventory = async () => {
   const snapshot = query(collection(firestore, 'inventory'))
   const docs = await getDocs(snapshot)
   const inventoryList = []
-  docs.forEach((doc) =>{
+  docs.forEach((doc) => {
     inventoryList.push({
       name: doc.id,
       ...doc.data(),
@@ -23,7 +23,7 @@ const updateInventory = async () => {
   setInventory(inventoryList)
 }
 
-const addItem = async (item) =>{
+const addItem = async (item) => {
   const docRef = doc(collection(firestore, 'inventory'), item)
   const docSnap = await getDoc(docRef)
 
@@ -37,7 +37,7 @@ const addItem = async (item) =>{
   await updateInventory()
 }
 
-const removeItem = async (item) =>{
+const removeItem = async(item) =>{
   const docRef = doc(collection(firestore, 'inventory'), item)
   const docSnap = await getDoc(docRef)
 
@@ -57,19 +57,120 @@ useEffect(()=>{
   updateInventory()
 }, [])
 
+const handleOpen = () => setOpen(true)
+const handleClose = () => setOpen(false)
+
   return (
-  <Box>
-    <Typography variant="h1">Inventory Management</Typography>
-    {
-      inventory.forEach((item) => {
-        console.log(item)
-        return (
-          <Box>
-        {item.name}
-        {item.count}
-        </Box>
-        )
-      })}
-  </Box>
+  <Box
+    width="100vw"
+    height="100vh"
+    display="flex"
+    flexDirection="column"
+    justifyContent="center"
+    alignItems="center"
+    gap={2}
+  >
+    <Modal open={open} onClose={handleClose}>
+      <Box
+        position="absolute"
+        top="50%"
+        left="50%"
+        width={400}
+        bgcolor="white"
+        border="2px solid #000"
+        boxShadow={24}
+        p={4}
+        display="flex"
+        flexDirection="column"
+        gap={3}
+        sx={{
+          transform: "translate(-50%,-50%)",
+        }}
+      >
+        <Typography variant="h6">Add Item</Typography>
+        <Stack width="100%" direction="row" spacing={2}>
+          <TextField
+            variant="outlined"
+            fullWidth
+            value={itemName}
+            onChange={(e)=>{
+              setItemName(e.target.value)
+            }}
+          />
+          <Button
+            variant="outlined"
+            onClick={()=>{
+              addItem(itemName)
+              setItemName('')
+              handleClose()
+            }}
+          >
+            Add
+          </Button>
+        </Stack>
+      </Box>
+    </Modal>
+    <Button
+      variant="contained"
+      onClick={() => {
+        handleOpen()
+      }}
+    >
+      Add New Item
+    </Button>
+    <Box border="1px solid #333">
+      <Box
+        width="800px"
+        height="100px"
+        bgcolor="#ADD8E6"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Typography variant="h2" color="#333">
+          Inventory Items
+        </Typography>
+      </Box>
+    <Stack width="800px" height="300px" spacing={2} overflow="auto">
+      {inventory.map(({name, quantity}) => {
+          <Box
+            key={name}
+            width="100%"
+            minHeight="150px"
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+            bgcolor="#f0f0f0"
+            padding={5}
+          >
+            <Typography variant="h3" color="#333" textAlign="center">
+              {name.charAt(0).toUpperCase() + name.slice(1)}
+            </Typography>
+            <Typography variant="h3" color="#333" textAlign="center">
+              {quantity}
+            </Typography>
+            <Stack direction="row" spacing={2}>
+            <Button
+              variant="contained"
+              onClick={() => {
+                addItem(name)
+            }}
+          >
+              Add
+            </Button>
+            <Button
+              variant="contained"
+              onClick={() => {
+                removeItem(name)
+            }}
+          >
+              Remove
+            </Button>
+            </Stack>
+          </Box>
+        })}
+      </Stack>
+      </Box>
+    </Box>
   )
 }
