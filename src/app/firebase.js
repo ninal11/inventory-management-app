b040094de07fc653eafbd0b7254187d4d4c556be
Nina Le <ninal11@uci.edu>
2024-08-01 19:02:10 -0700

    Update page.js

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,5 +1,6 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app'
+import {getFirestore} from 'firebase/firestore'
+
 const firebaseConfig = {
  apiKey: "AIzaSyA8yJ-j9QbcPmRaC3aJYPaAF8zf5rY6UCU",
  authDomain: "inventory-management-app-305da.firebaseapp.com",
@@ -7,7 +8,9 @@ const firebaseConfig = {
  storageBucket: "inventory-management-app-305da.appspot.com",
  messagingSenderId: "288583659723",
  appId: "1:288583659723:web:f384fe8f4ec64ffaefeb4e"
- };
-const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app);
-export { firestore };
+ }
+
+const app = initializeApp(firebaseConfig)
+const firestore = getFirestore(app)
+
+export {firestore};
